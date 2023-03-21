487ad2b4c88449df91c60fa3157f6ca5ade9be36
Horizon\XinjieDang <1328791209@qq.com>
2023-03-21 21:17:43 +0800

    update

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `DxJay's Blog`
+export const SITE_DESCRIPTION = 'DxJay Site Description'
+export const SITE_EMAIL = ''
+export const SITE_NAME = ''
+export const SITE_URL = 'https://astro.yufengbiji.com'
